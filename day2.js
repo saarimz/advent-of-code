92deb0e7f230b843e0bf685177743e79237e0684
Saarim Zaman <saarim.z@gmail.com>
2017-12-03 23:59:00 -0500

    completed day 2

--- day2.js
+++ day2.js
@@ -11,3 +11,25 @@ function checksum(sheet) {
 
 //PART TWO
 
+function checksum_2(sheet) {
+  const formatted_input = sheet.split("\n").map((row) => (row.split("\t").map(Number)));
+  
+  const get_divisible = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = i + 1; j < arr.length; j++ ) {
+        if (arr[i] % arr[j] === 0){
+          return (arr[i] / arr[j]);
+        } 
+        else if (arr[j] % arr[i] === 0) {
+          return (arr[j] / arr[i]);
+        }
+      }
+    }
+    return 0;
+  };
+  
+  return formatted_input.reduce((acc,row,index,arr) => {
+    acc += get_divisible(row);
+    return acc;
+  },0);
+}
