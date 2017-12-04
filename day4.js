bb0857af2b17ebee65ff43c4efcf1900140c9fcb
Saarim Zaman <saarim.z@gmail.com>
2017-12-04 16:47:35 -0500

    completed day4.js

--- day4.js
+++ day4.js
@@ -19,3 +19,21 @@ function passphrase(input) {
 
 //PART TWO
 
+function passphrase_2(input) {
+  
+  const validator = (arr) => {
+    return arr.split(' ').reduce((acc,val,index,arr) => {
+      val = val.split('').sort().join('');
+      let remaining_array = arr.slice(index+1);
+      for (let i = 0; i < remaining_array.length; i++) {
+        remaining_array[i] = remaining_array[i].split('').sort().join('');
+        if (val === remaining_array[i]) {
+          acc = false;
+        }
+      }
+      return acc;
+    },true);
+  };
+  
+  return input.split("\n").map(validator).filter(Boolean).length;  
+}
