928e8a9243cc16b480108e0b51021547285a1fcc
Saarim Zaman <saarim.z@gmail.com>
2017-12-03 23:32:34 -0500

    completed part 1

--- day2.js
+++ day2.js
@@ -1,1 +1,13 @@
 // http://adventofcode.com/2017/day/2
+
+//PART ONE
+function checksum(sheet) {
+  const formatted_input = sheet.split("\n").map((row) => (row.split("\t").map(Number)));
+  return formatted_input.reduce((acc,val,index,arr) => {
+    acc += (Math.max(...val) - Math.min(...val));
+    return acc;
+  },0)
+}
+
+//PART TWO
+
