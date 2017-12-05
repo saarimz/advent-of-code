05de55159a77fb8905fb1d433e9682d06f577424
Saarim Zaman <saarim.z@gmail.com>
2017-12-04 21:30:06 -0500

    updated part I

--- day3.js
+++ day3.js
@@ -1,1 +1,12 @@
 // http://adventofcode.com/2017/day/3
+
+//PART ONE (used a lot of help from the advent reddit to get this)
+
+function distance(n) {
+    const root = Math.ceil(Math.sqrt(n));
+    const corner_square = root % 2 !== 0 ? root : root + 1;
+    const num = (corner_square - 1) / 2;
+    const cycle = n - Math.pow((corner_square - 2),2);
+    const innerOffset = cycle % (corner_square - 1);
+    return num + Math.abs(innerOffset - num);
+}
