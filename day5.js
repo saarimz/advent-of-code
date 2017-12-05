ec16c3a8b2d16ef2583d54a96471d0605d1a4b8f
Saarim Zaman <saarim.z@gmail.com>
2017-12-05 11:46:40 -0500

    finished part one

---
+++ day5.js
@@ -0,0 +1,18 @@
+// http://adventofcode.com/2017/day/5/
+
+// part one
+
+function count_steps(input_arr) {
+  input_arr = input_arr.split('\n').map(Number);
+  let steps = 0;
+  let current_index = 0;
+  while ((current_index < input_arr.length) && (current_index >= 0)) {
+    let move = input_arr[current_index]
+    input_arr[current_index] += 1;
+    current_index += move;
+    steps++;
+  }
+  return steps;
+}
+
+// part two
