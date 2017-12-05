c6bb9db08fe9137d844f2c3612a56c0fd21e480b
Saarim Zaman <saarim.z@gmail.com>
2017-12-05 18:53:16 -0500

    finished day5, fixed formatting

--- day5.js
+++ day5.js
@@ -3,7 +3,7 @@
 // part one
 
 function count_steps(input_arr) {
-  input_arr = input_arr.split('\n').map(Number);
+  let input_arr = input_arr.split('\n').map(Number);
   let steps = 0;
   let current_index = 0;
   while ((current_index < input_arr.length) && (current_index >= 0)) {
@@ -18,12 +18,12 @@ function count_steps(input_arr) {
 // part two
 
 function count_steps_2(input_arr) {
-  input_arr = input_arr.split('\n').map(Number);
+  let input_arr = input_arr.split('\n').map(Number);
   let steps = 0;
   let current_index = 0;
   while ((current_index < input_arr.length) && (current_index >= 0)) {
-  input_arr[current_index] >= 3 ? (current_index += input_arr[current_index]--) : (current_index += input_arr[current_index]++)
-  steps++
+    input_arr[current_index] >= 3 ? (current_index += input_arr[current_index]--) : (current_index += input_arr[current_index]++);
+    steps++;
   }
   return steps;
 }
